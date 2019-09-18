bd894657508ea56f6397c16f6f8a5b71dee38e84
madness4love <lokatin21@gmail.com>
2019-09-18 22:57:28 +0300

    add greeting

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ start:
 	npx babel-node src/bin/brain-games.js
 publish:
 	npm publish --dry-run
+edit:
+	nano ./src/bin/brain-games.js
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import readlineSync from '..';
 console.log("Welcome to the Brain Games!");
+const name = readlineSync.question('May i have your name? ');
+console.log(`Hello, ${name}!`);
