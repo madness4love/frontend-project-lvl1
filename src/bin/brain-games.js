4fd8c9a48fe6bb7a3d20b7bba3533059644bb68b
madness4love <lokatin21@gmail.com>
2019-10-04 19:53:44 +0300

    add rule of the game

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,5 +2,6 @@
 import readlineSync from '..';
 
 console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const name = readlineSync.question('May i have your name? ');
 console.log(`Hello, ${name}!`);
