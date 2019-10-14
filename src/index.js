d804f4dd21d1c6ed10743a8b15b9e75c1586d515
madness4love <lokatin21@gmail.com>
2019-10-14 23:09:29 +0300

    error correction

--- src/index.js
+++ src/index.js
@@ -1,40 +1,35 @@
 import readlineSync from 'readline-sync';
+import { greeting } from './greeting';
+import { answer } from './answer';
+import { identity } from './identity';
 
 export default readlineSync;
-//rules
+// rules
 export const welcome = () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 };
-//rules
+// rules
 
-//greeting
-const greeting = () => {
-  return readlineSync.question('May I have your name? ');
-};
-export const hello = () => {
-  console.log(`Hello, ${greeting()}`);
-};
-//greeting
-
-//game
-
+// game
 const isEven = num => (num % 2 === 0) ? 'yes' : 'no';
 
 export const question = () => {
+  const name = identity(greeting());
+  console.log(`Hello, ${name}`);
+
   for (let i = 1; i < 4; i += 1) {
     const randomNum = Math.ceil(Math.random() * 10);
     console.log(`Question: ${randomNum}`);  
-    const answer = () => {
-      return readlineSync.question('Your answer: ');
-    };
-    if (`${answer()}` === isEven(randomNum)) {
+    const ans = identity(answer());
+    
+    if (`${ans}` === isEven(randomNum)) {
       console.log('Correct!');
       if (i === 3) {
-        return console.log(`Congratulations, ${greeting()}!`)
+        return console.log(`Congratulations, ${name}!`)
       }
     } else {
-      return console.log(`'${answer()}' is wrong answer ;(. Correct answer was '${isEven()}'.\n Let's try again, ${greeting()}!`)
+      return console.log(`'${ans}' is wrong answer ;(. Correct answer was '${isEven(randomNum)}'.\n Let's try again, ${name}!`)
     }
   }
 };
