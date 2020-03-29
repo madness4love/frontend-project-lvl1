b4f57bf21df557ac7b92a55eaa3ab20273861735
madness4love <lokatin21@gmail.com>
2020-03-29 23:20:00 +0300

    fix prime game

--- src/games/primeGameData.js
+++ src/games/primeGameData.js
@@ -4,29 +4,29 @@ import getGameEngine from '..';
 
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const getRightAnswer = (number) => {
+  let answer = '';
+  if (number < 4) {
+    return 'yes';
+  }
+  const isPrime = (divisor) => {
+    const div = divisor;
+    if (number % div === 0) {
+      answer = 'no';
+      return answer;
+    }
+    if (div === 2 && number % div !== 0) {
+      answer = 'yes';
+      return answer;
+    }
+    return isPrime(div - 1);
+  };
+  isPrime(Math.floor(number / 2));
+  return answer;
+};
+
 const getGameData = () => {
   const randNum = getRandomNum(1, 100);
-
-  const getRightAnswer = (number) => {
-    let answer = '';
-    if (number < 4) {
-      return 'yes';
-    }
-    const isPrime = (divisor) => {
-      const div = divisor;
-      if (number % div === 0) {
-        answer = 'no';
-        return answer;
-      }
-      if (div === 2 && number % div !== 0) {
-        answer = 'yes';
-        return answer;
-      }
-      return isPrime(div - 1);
-    };
-    isPrime(Math.floor(number / 2));
-    return answer;
-  };
 
   const rightAnswer = getRightAnswer(randNum);
   const question = `${randNum}`;
