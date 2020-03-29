829b71064b2453a6bfaa10b1c6cefd79cb56bccd
madness4love <lokatin21@gmail.com>
2020-03-29 23:18:22 +0300

    fix gcd game

--- src/games/gcdGameData.js
+++ src/games/gcdGameData.js
@@ -4,20 +4,20 @@ import getGameEngine from '..';
 
 const rule = 'Find the greatest common divisor of given numbers.';
 
+const getRightAnswer = (n1, n2) => {
+  const geatestNum = Math.max(n1, n2);
+  const smallestNum = Math.min(n1, n2);
+  const result = geatestNum % smallestNum;
+
+  if (result === 0) {
+    return smallestNum;
+  }
+  return getRightAnswer(smallestNum, result);
+};
+
 const getGameData = () => {
   const randNum1 = getRandomNum(10, 100);
   const randNum2 = getRandomNum(5, 50);
-
-  const getRightAnswer = (n1, n2) => {
-    const geatestNum = Math.max(n1, n2);
-    const smallestNum = Math.min(n1, n2);
-    const result = geatestNum % smallestNum;
-
-    if (result === 0) {
-      return smallestNum;
-    }
-    return getRightAnswer(smallestNum, result);
-  };
 
   const rightAnswer = getRightAnswer(randNum1, randNum2);
   const question = `${randNum1} ${randNum2}`;
