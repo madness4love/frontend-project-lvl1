e572e4d9f6cece38f64ed23d63871d6fc4a683e4
madness4love <lokatin21@gmail.com>
2020-04-02 22:11:55 +0300

    refactor progression game

--- src/games/progressionGameData.js
+++ src/games/progressionGameData.js
@@ -6,52 +6,24 @@ const rule = 'What number is missing in the progression?';
 
 const getProgression = (start, step) => {
   let result = String(start);
-  let index = 10;
-  let previousNumber = start;
-  let nextNumber = previousNumber + step;
+  let index = 1;
+  const numberOfDigit = 10;
 
-  while (index > 0) {
+  while (index < numberOfDigit) {
+    const nextNumber = start + step * index;
     result += ` ${nextNumber}`;
-    index -= 1;
-    previousNumber = nextNumber;
-    nextNumber = previousNumber + step;
+    index += 1;
   }
   return result;
-};
-
-
-const getRightAnswer = (string) => {
-  const isProgression = string;
-  let answer = '';
-  const index = getRandomNum(1, isProgression.length - 1);
-
-  const getStartPositionAndAnswer = (n) => {
-    let position = isProgression[n];
-    let i = n;
-    if (position === ' ') {
-      position = isProgression[i + 1];
-      while (position !== ' ') {
-        i += 1;
-        position = isProgression[i];
-        if (position === ' ') {
-          return answer;
-        }
-        answer += position;
-      }
-    }
-    return getStartPositionAndAnswer(i + 1);
-  };
-
-  getStartPositionAndAnswer(index);
-  return answer;
 };
 
 const getGameData = () => {
   const randNum1 = getRandomNum(1, 100);
   const randNum2 = getRandomNum(2, 50);
+  const index = getRandomNum(1, 10);
   const progression = getProgression(randNum1, randNum2);
 
-  const rightAnswer = getRightAnswer(progression);
+  const rightAnswer = randNum1 + randNum2 * index;
   const question = progression.replace(rightAnswer, '..');
   return cons(question, rightAnswer);
 };
