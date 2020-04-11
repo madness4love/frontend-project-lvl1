62a39d70e9169deee441b76ae5990169a1480181
madness4love <lokatin21@gmail.com>
2020-04-11 20:44:39 +0300

    refractor even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,5 @@
 import { cons } from '@hexlet/pairs';
-import getRandomNum from '../utils';
+import getQuestionNumber from '../utils';
 import getGameEngine from '..';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
@@ -7,7 +7,7 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = num => num % 2 === 0;
 
 const getGameData = () => {
-  const randomNum = getRandomNum(1, 100);
+  const randomNum = getQuestionNumber(1, 100);
 
   const rightAnswer = isEven(randomNum) ? 'yes' : 'no';
 
