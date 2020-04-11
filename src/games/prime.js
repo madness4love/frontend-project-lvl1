b696e3ba5e584baa85ce13378a7136aa6879932d
madness4love <lokatin21@gmail.com>
2020-04-11 23:13:49 +0300

    refractor prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,34 +4,27 @@ import getGameEngine from '..';
 
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const getRightAnswer = (number) => {
-  let answer = '';
-  if (number < 4) {
-    return 'yes';
+// eslint-disable-next-line consistent-return
+const isPrime = (num) => {
+  let div = Math.floor(num / 2);
+
+  while (div >= 2) {
+    if (num % div === 0) {
+      return false;
+    }
+    if (div === 2 && num % div !== 0) {
+      return true;
+    }
+    div -= 1;
   }
-  const isPrime = (divisor) => {
-    const div = divisor;
-    if (number % div === 0) {
-      answer = 'no';
-      return answer;
-    }
-    if (div === 2 && number % div !== 0) {
-      answer = 'yes';
-      return answer;
-    }
-    return isPrime(div - 1);
-  };
-  isPrime(Math.floor(number / 2));
-  return answer;
 };
 
 const getGameData = () => {
   const randNum = getRandomNum(1, 100);
 
-  const rightAnswer = getRightAnswer(randNum);
+  const rightAnswer = isPrime(randNum) ? 'yes' : 'no';
   const question = `${randNum}`;
   return cons(question, rightAnswer);
 };
-
 
 export default () => getGameEngine(getGameData, rule);
