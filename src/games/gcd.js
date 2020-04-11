04b42a13865e97ff8001f9f5d6cdbf61d6783d3c
madness4love <lokatin21@gmail.com>
2020-04-11 22:28:11 +0300

    refractor gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,10 +1,10 @@
-import { cons } from '@hexlet/pairs';
+import { cons, cdr, car } from '@hexlet/pairs';
 import getRandomNum from '../utils';
 import getGameEngine from '..';
 
 const rule = 'Find the greatest common divisor of given numbers.';
 
-const getRightAnswer = (n1, n2) => {
+/* const getRightAnswer = (n1, n2) => {
   const geatestNum = Math.max(n1, n2);
   const smallestNum = Math.min(n1, n2);
   const result = geatestNum % smallestNum;
@@ -13,13 +13,26 @@ const getRightAnswer = (n1, n2) => {
     return smallestNum;
   }
   return getRightAnswer(smallestNum, result);
+}; */
+
+const getGcd = (n1, n2) => {
+  const geatestNum = Math.max(n1, n2);
+  const smallestNum = Math.min(n1, n2);
+
+  const pairOfNumbers = cons(geatestNum, smallestNum);
+  const result = car(pairOfNumbers) % cdr(pairOfNumbers);
+
+  if (result === 0) {
+    return cdr(pairOfNumbers);
+  }
+  return getGcd(cdr(pairOfNumbers), result);
 };
 
 const getGameData = () => {
   const randNum1 = getRandomNum(10, 100);
   const randNum2 = getRandomNum(5, 50);
 
-  const rightAnswer = getRightAnswer(randNum1, randNum2);
+  const rightAnswer = getGcd(randNum1, randNum2);
   const question = `${randNum1} ${randNum2}`;
   return cons(question, rightAnswer);
 };
