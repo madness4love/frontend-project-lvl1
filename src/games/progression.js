87f5d113da48dce76426830eba1802f326de9eb4
madness4love <lokatin21@gmail.com>
2020-04-11 22:39:47 +0300

    refractor progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,26 +4,25 @@ import getGameEngine from '..';
 
 const rule = 'What number is missing in the progression?';
 
+const countOfDigitInProgression = 10;
+
 const getProgression = (start, step) => {
   let result = String(start);
-  let index = 1;
-  const numberOfDigit = 10;
 
-  while (index < numberOfDigit) {
+  for (let index = 1; index < countOfDigitInProgression; index += 1) {
     const nextNumber = start + step * index;
-    result += ` ${nextNumber}`;
-    index += 1;
+    result = `${result} ${nextNumber}`;
   }
   return result;
 };
 
 const getGameData = () => {
-  const randNum1 = getRandomNum(1, 100);
-  const randNum2 = getRandomNum(2, 50);
-  const index = getRandomNum(1, 10);
-  const progression = getProgression(randNum1, randNum2);
+  const startDigit = getRandomNum(1, 100);
+  const step = getRandomNum(2, 50);
+  const hideElementPosition = getRandomNum(1, countOfDigitInProgression);
+  const progression = getProgression(startDigit, step);
 
-  const rightAnswer = randNum1 + randNum2 * index;
+  const rightAnswer = startDigit + step * hideElementPosition;
   const question = progression.replace(rightAnswer, '..');
   return cons(question, rightAnswer);
 };
